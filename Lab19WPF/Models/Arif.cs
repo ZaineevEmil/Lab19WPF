04ad8c3aa2e33e4773cee09db3a4c7d833ddde92
2299112@gmail.com <2299112@gmail.com>
2022-01-21 20:43:18 +0500

    Добавьте файлы проекта.

---
+++ Lab19WPF/Models/Arif.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19WPF.Models
+{
+    static class Arif
+    {
+        public static double Add(double a) => Math.PI * a * 2;
+    }
+}
---
+++ Lab19WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Lab19WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab19WPF.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged ([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double circle;
+        public double Сircle
+        {
+            get => circle;
+            set
+            {
+                circle = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Сircle = Arif.Add(Radius);
+        }
+
+        private bool CanAddCommandExecute(object p)
+        {
+            if (Radius > 0) return true;
+            else return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+
+    }
+}
